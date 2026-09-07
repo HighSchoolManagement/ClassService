ef58e061e8f53ff573861f424a8e3c8bd8ca08b7
Maïa PERDERIZET <dev.info@capronpodologie.com>
2026-09-07 17:53:06 +0200

    first commit

---
+++ src/ClassService.Domain/Entities/ClassTeacherAssignment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassService.Domain.Entities
+{
+    public class ClassTeacherAssignment
+    {
+    }
+}
---
+++ src/ClassService.Domain/Entities/Enrollment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassService.Domain.Entities
+{
+    internal class Enrollment
+    {
+    }
+}
