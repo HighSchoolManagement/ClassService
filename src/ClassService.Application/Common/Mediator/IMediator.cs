c8102321c037ee961b28713bac675fe5693e3d37
Trong Dat <datnt700@gmail.com>
2026-09-09 22:00:18 +0200

    send method

--- src/ClassService.Application/Common/Mediator/IMediator.cs
+++ src/ClassService.Application/Common/Mediator/IMediator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ClassService.Application.Common.Mediator;
 
 namespace ClassService.Application.Mediator
 {
--- src/ClassService.Application/Common/Mediator/Mediator.cs
+++ src/ClassService.Application/Common/Mediator/Mediator.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ClassService.Application.Common.Mediator;
 
 namespace ClassService.Application.Mediator
 {
-    internal class Mediator
+    public class Mediator : IMediator
     {
+        private readonly IServiceProvider _serviceProvider;
+
+        public Mediator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            var requestType = request.GetType();
+            var handlerType = typeof(IRequestHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+            return null;
+        }
     }
 }
