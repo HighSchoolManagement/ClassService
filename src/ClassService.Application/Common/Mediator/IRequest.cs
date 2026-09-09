a96453acca6742c4a6f391a35b6e35c47ccff228
Maïa PERDERIZET <dev.info@capronpodologie.com>
2026-09-09 17:45:10 +0200

    feat:mediator

---
+++ src/ClassService.Application/Common/Mediator/IRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassService.Application.Common.Mediator
+{
+    public interface IRequest<TResponse>
+    {
+    }
+}
